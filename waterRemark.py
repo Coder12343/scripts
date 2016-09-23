4eb632afdec8c70c0e6cd30f649c9db18cfdd0c2
Coder12343 <huangtianqi@shule.com>
2016-09-23 10:17:58 +0800

    add waterRemark script

---
+++ waterRemark.py
@@ -0,0 +1,62 @@
+from PIL import Image, ImageEnhance
+def reduce_opacity(im, opacity):
+  """Returns an image with reduced opacity."""
+  assert opacity >= 0 and opacity <= 1
+  if im.mode != 'RGBA':
+    im = im.convert('RGBA')
+  else:
+    im = im.copy()
+  alpha = im.split()[3]
+  alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+  im.putalpha(alpha)
+  return im
+def watermark(im, mark, position, opacity=1):
+  """Adds a watermark to an image."""
+  if opacity < 1:
+    mark = reduce_opacity(mark, opacity)
+  if im.mode != 'RGBA':
+    im = im.convert('RGBA')
+  # create a transparent layer the size of the image and draw the
+  # watermark in that layer.
+  layer = Image.new('RGBA', im.size, (0,0,0,0))
+  if position == 'tile':
+    for y in range(0, im.size[1], mark.size[1]):
+      for x in range(0, im.size[0], mark.size[0]):
+        layer.paste(mark, (x, y))
+  elif position == 'scale':
+    # scale, but preserve the aspect ratio
+    ratio = min(
+      float(im.size[0]) / mark.size[0], float(im.size[1]) / mark.size[1])
+    w = int(mark.size[0] * ratio)
+    h = int(mark.size[1] * ratio)
+    mark = mark.resize((w, h))
+    layer.paste(mark, (int((im.size[0] - w) / 2), int((im.size[1] - h) / 2)))
+  else:
+    layer.paste(mark, position)
+  # composite the watermark with the layer
+  return Image.composite(layer, im, layer)
+def get_pictures():
+    contents=open("waterRemark.txt",'r+')
+    content=contents.readlines()
+    result={}
+    for c in content:
+        if(c.split(":")[0]=="remark"):
+            result["mark"]=c.split(":")[1][:-1]
+        elif(c.split(":")[0]=="pictures"):
+            result["pictures"]=c.split(":")[1].split(",")
+    # print(result["mark"]+"   "+result["pictures"])
+    return result
+
+def test():
+  # im = Image.open('ico.png')
+  # mark = Image.open('box.png')
+  pi=get_pictures()
+  mark=Image.open(pi["mark"])
+  ims=pi["pictures"]
+  for path in ims:
+      im=Image.open(path)
+      watermark(im, mark, 'tile', 0.5).show()
+      watermark(im, mark, 'scale', 0.5).show()
+      watermark(im, mark, (100, 100), 0.5).show()
+if __name__ == '__main__':
+  test()
